e132d67f56e4674119f688eda88b455e1cfef9bd
Lucai994 <luca.muratori.94@hotmail.it>
2022-04-16 11:30:22 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Component from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component() {
+  state = {
+    media: [],
+  };
+
+  componentDidMount = async () => {
+    const apiUrl = process.env.REACT_APP_URL;
+    const resp = await fetch(`${apiUrl}/media`);
+    const jsonMedia = await resp.json();
+    this.setState({ media: jsonMedia });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div>
+          {this.state.media.map((media) => (
+            <div key={media.id}>
+              <div>{media.Title}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
